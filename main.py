7ffb154b9eb667d82d933a351d6ff5fba1f791a8
r-jrcp12 <correo con el que se registr en Github>
2026-03-13 15:23:16 -0500

    busqueda de especie

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import requests
+
+def invasive_species(num):
+  url = f"https://api-colombia.com/api/v1/Invasivespecie/{num}"
+
+  response  = requests.get(url)
+  data = response.json()
+
+  information = {
+    "id": data["id"],
+    "name": data["name"],
+    "scientificName": data["scientificName"],
+    "impact": data["impact"]
+  }
+  
+
+  return information
+
+
+
+
+
 def main():
-  print("Hello learners!")
+  print("Hello, students!")
+
+  num = int(input("Numero de especie: "))
+  resultado = invasive_species(num)
+  print(resultado)
 
 if __name__=="__main__":
+
   main()
